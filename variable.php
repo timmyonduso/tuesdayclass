66963eb0ea34472c3f1a0b04b1fbb3586ffec396
Millicent-Wangui <millywangui9@gmail.com>
2022-06-15 16:10:11 +0300

    Initial commit

---
+++ variable.php
@@ -0,0 +1,35 @@
+<?php
+
+//variables
+
+$majina="kinder joy";
+$nambari= 54;
+
+echo "My name is $majina";
+echo "<br>";
+echo "I am $nambari years old";
+echo "<br>";
+
+$num1=20;
+$num2=22;
+
+echo "$num1+$num2 is <br>";
+echo $num1+$num2;
+echo "<br>";
+
+echo "$num1-$num2 is <br>";
+echo $num1-$num2;
+echo "<br>";
+
+echo "$num1*$num2 is<br>";
+echo $num1*$num2;
+//echo "<br>";
+
+echo "$num1/$num2 is <br>";
+echo $num1/$num2;
+echo "<br>";
+
+echo  "sum is ".$num1 + $num2;
+
+?>
+
